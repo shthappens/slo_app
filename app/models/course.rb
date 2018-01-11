918c27396f4cf819d878d6d2758951d060eb382e
Seth Thompson <seth.h.thompson@gmail.com>
2018-01-11 14:52:12 -0500

    removed unnecessary controllers

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,6 @@
 class Course < ApplicationRecord
+  has_many :teachers, through :teachercourses
+  has_many :students, through :studentcourses
+
+  validates :name, presence: true, allow_blank: false
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   get 'slo_app/index'
 
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root "slo_app#index"
+
+  resources :users, only: [:index]
+  devise_for :users
+
 
 end
